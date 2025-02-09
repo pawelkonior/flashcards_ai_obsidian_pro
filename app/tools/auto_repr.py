0f8d3393fc56e80169e6861146c2194f8503f17e
Paweł Konior <pawel.konior@gmail.com>
2025-02-09 15:46:15 +0100

    awesome autorepr class decorator

---
+++ app/tools/auto_repr.py
@@ -0,0 +1,32 @@
+import inspect
+from types import MappingProxyType
+from typing import TypeVar, Any
+
+T = TypeVar('T', bound=type[Any])
+
+
+def auto_repr(cls: T) -> T:
+    members: MappingProxyType[str, Any] = vars(cls)
+
+    if "__repr__" in members:
+        raise TypeError(f'{cls.__name__} already has __repr__ method.')
+
+    if "__init__" not in members:
+        raise TypeError(f'{cls.__name__} does not override __init__ method.')
+
+    signature: inspect.Signature = inspect.signature(cls.__init__)
+    parameter_names: list[str] = list(signature.parameters)[1:]
+
+    if not all(members.get(name, None) for name in parameter_names):
+        raise TypeError(f'Cannot apply auto_repr to {cls.__name__} because not all'
+                        f'__init__ parameters have matching properties.')
+
+    def synthesized_repr(self: T) -> str:
+        return "{type_class}({args})".format(
+            type_class=type(self).__name__,
+            args=", ".join(f'{name}={getattr(self, name)!r}' for name in parameter_names)
+        )
+
+    setattr(cls, "__repr__", synthesized_repr)
+
+    return cls
